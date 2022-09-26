61b1995636cfe336331beb25bbbb67003636dbd3
Robert Teets <teetsrobert@gmail.com>
2022-09-26 11:39:45 -0600

    2: cleared out default react stuff

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h2>This is a test title.</h2>
+      <p>Lorem ipsum.</p>
+      <h2>This is a test as well.</h2>
+      <p>Lorem ipsum.</p>
+    </div>
+  );
+}
+
+export default App;
